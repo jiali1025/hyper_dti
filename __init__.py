4fa02d02969a01fdb94720a13e752ee7be901d47
jiali1025 <lijiali1994@gmail.com>
2023-03-14 17:00:23 +0800

    Delete __init__.py

--- __init__.py
+++
@@ -1,6 +0,0 @@
-from .adapter_outputs import (SamplerOutput, LayerNormOutput,
-                              AdapterDTIBlockOutput, AdapterOutput)
-from .multihead_attention import MultiheadAttention
-from .esm_data import Alphabet
-from .modules import ContactPredictionHead, ESM1bLayerNorm, RobertaLMHead
-from .Protein_model import DTI, MetaAdapterConfig
